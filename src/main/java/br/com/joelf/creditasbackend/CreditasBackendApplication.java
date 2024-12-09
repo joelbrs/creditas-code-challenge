5a83c203347ac76dc8becaa59add7d67fa30d503
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2024-12-09 16:22:51 -0300

    chore: init project

---
+++ src/main/java/br/com/joelf/creditasbackend/CreditasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.creditasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joelf/creditasbackend/CreditasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelf.creditasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
